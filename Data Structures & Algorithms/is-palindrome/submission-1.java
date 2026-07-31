2f04fa98586d3b4b0d5f33aac03cacc54e268b2b
TrucDiep <111523555+hotrucdiep21@users.noreply.github.com>
2026-07-31 20:33:51 +0700

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r) {
+            if (!Character.isLetter(s.charAt(l)) && !Character.isDigit(s.charAt(l))) {
+                l++;
+                continue;
+            }
+            if (!Character.isLetter(s.charAt(r)) && !Character.isDigit(s.charAt(r))) {
+                r--;
+                continue;
+            }
+
+            if (Character.toLowerCase(s.charAt(l)) == Character.toLowerCase(s.charAt(r))) {
+                l++;
+                r--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
