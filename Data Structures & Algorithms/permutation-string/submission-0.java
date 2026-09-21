05affa6521c6a25ac54009615c38435a9704ed89
TrucDiep <111523555+hotrucdiep21@users.noreply.github.com>
2026-09-21 10:59:18 +0700

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int windowSize = s1.length();
+        if (windowSize > s2.length()) {
+            return false;
+        }
+        int[] s1Count = new int[26];
+        int[] windowCount = new int[26];
+
+        for (int i = 0; i < windowSize; i++) {
+            s1Count[s1.charAt(i) - 'a']++;
+            windowCount[s2.charAt(i) - 'a']++;
+        }
+
+        if (Arrays.equals(s1Count, windowCount)) {
+            return true;
+        }
+
+        for (int right = windowSize; right < s2.length(); right++) {
+            windowCount[s2.charAt(right) - 'a']++;
+
+            windowCount[s2.charAt(right - windowSize) - 'a']--;
+            if (Arrays.equals(s1Count,windowCount)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
